ce2b12912e37f7322f05126a28acea576eb10376
Somesh Math <248798024+somumath7-ops@users.noreply.github.com>
2026-05-11 19:41:56 +1000

    zowe version line removed

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -9,8 +9,6 @@ export PATH=$PATH:$(npm config get prefix)/bin
 # 2. Prevent Git Bash from remapping USS paths (useful for Windows users)
 export MSYS_NO_PATHCONV=1
 
-zowe --version
-
 # Convert username to lowercase
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 # Check if directory exists, create if it doesn't
