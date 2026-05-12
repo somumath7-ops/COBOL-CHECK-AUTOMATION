23eba86988f6287bb1fee30e28eb02a698321d9c
somumath7-ops <248798024+somumath7-ops@users.noreply.github.com>
2026-05-12 12:55:10 +1000

    Simplify zowe upload command by merging lines

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -28,8 +28,7 @@ else
 echo "Directory already exists."
 fi
 # Upload files
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive
-        --binary-files "cobol-check-0.2.9.jar"
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.9.jar"
 # Verify upload
 echo "Verifying upload:"
 zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
